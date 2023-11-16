e539597d14470d6710349877ac6507be238af1f1
moeinparvizi <giventulip@gmail.com>
2023-11-16 17:30:11 +0330

    add images and social html

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -1,3 +1,8 @@
+// import images
+import githubImage from '../../../public/github.png'
+import instagramImage from '../../../public/instagram.png'
+import linkedinImage from '../../../public/linkedin.webp'
+import stackoverflowImage from '../../../public/stackoverflow.png'
 
 import './navbar.scss'
 
@@ -9,7 +14,10 @@ export default function Navbar() {
       <div className="wrapper">
         <span>filan</span>
         <div className='social'>
-          <a href=""><img src="" alt="" /></a>
+          <a href=""><img src={githubImage} alt="github" /></a>
+          <a href=""><img src={instagramImage} alt="instagram" /></a>
+          <a href=""><img src={linkedinImage} alt="linkedin" /></a>
+          <a href=""><img src={stackoverflowImage} alt="stackoverflow" /></a>
         </div>
       </div>
     </div>
