48b4cd5d46091dcbbd3861d978409b10d9ee97c5
moeinparvizi <giventulip@gmail.com>
2023-11-16 19:04:08 +0330

    add social and json and add motion for animation when loaded

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -1,26 +1,69 @@
 // import images
-import githubImage from '../../../public/github.png'
-import instagramImage from '../../../public/instagram.png'
-import linkedinImage from '../../../public/linkedin.webp'
-import stackoverflowImage from '../../../public/stackoverflow.png'
+import githubImage from "../../../public/github.png";
+import instagramImage from "../../../public/instagram.png";
+import linkedinImage from "../../../public/linkedin.webp";
+import stackoverflowImage from "../../../public/stackoverflow.png";
 
-import './navbar.scss'
+import "./navbar.scss";
 
+// librarys
+import { motion } from "framer-motion";
+
+// arrays
+const socials = [
+  {
+    name: 'github',
+    img: githubImage,
+    url: "https://github.com/moeinparvizi",
+  },
+  {
+    name: 'instagram',
+    img: instagramImage,
+    url: "https://instagram.com/moeinparvizi.dev",
+  },
+  {
+    name: 'linkedin',
+    img: linkedinImage,
+    url: "https://linkedin.com/in/moeinparvizi2002",
+  },
+  {
+    name: 'stackoverflow',
+    img: stackoverflowImage,
+    url: "https://linkedin.com/in/moeinparvizi2002",
+  },
+];
 export default function Navbar() {
+  const variants = {
+    visible: (i) => ({
+      opacity:1,
+      x:0,
+      transition:{delay: i * 0.5},
+    }),
+
+    hidden: { opacity: 0, x: -100 },
+  }
   return (
-	<>
-    <div className="navbar">
-      {/* slidebar */}
-      <div className="wrapper">
-        <span>filan</span>
-        <div className='social'>
-          <a href=""><img src={githubImage} alt="github" /></a>
-          <a href=""><img src={instagramImage} alt="instagram" /></a>
-          <a href=""><img src={linkedinImage} alt="linkedin" /></a>
-          <a href=""><img src={stackoverflowImage} alt="stackoverflow" /></a>
+    <>
+      <div className="navbar">
+        {/* slidebar */}
+        <div className="wrapper">
+          <motion.span
+            initial={{ opacity: 0, x: 100, scale: 0 }}
+            animate={{ opacity: 1, x: 0, scale: 1 }}
+          >
+            filan
+          </motion.span>
+          <motion.div variants={variants} initial='hidden' animate='visible' className="social">
+            {socials.map((item, i) => {
+              return(
+                <motion.a key={item.name} href={item.url} variants={variants} custom={i}>
+                  <img src={item.img} alt="logo" />
+                </motion.a>
+              )
+            })}
+          </motion.div>
         </div>
       </div>
-    </div>
-	</>
-  )
+    </>
+  );
 }
