ec5607cb2a1ba8ac140a950b5fa29b6a7a4a428d
moeinparvizi <giventulip@gmail.com>
2023-11-17 16:34:02 +0330

    finish parallax animation

--- src/Components/parallax/Parallax.jsx
+++ src/Components/parallax/Parallax.jsx
@@ -1,12 +1,42 @@
 // import librarys
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 // import styles
 import "./parallax.scss";
 
-export const Parallax = ({ type }) => {
+
+// const plan = {
+//   initial: {
+//     y: 0,
+//   },
+//   animate: {
+//     y: 10,
+//     transition: {
+//       repeat: Infinity,
+//       duration: 20,
+//       repeatType: "mirror",
+//       stiffness: 220,
+//       damping: 300,
+//       type: "spring"
+//     },
+//   },
+// };
+
+export const Parallax = ({ type }) => { const parallax = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: parallax,
+    offset: ["start start", "end start"],
+  });
+
+  const stars = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  const text = useTransform(scrollYProgress, [0, 1], ["0%", "500%"]);
+  const planets = useTransform(scrollYProgress, [0, 1], ["0%", "300%"]);
+
   return (
     <div
       className="parallax"
+      ref={parallax}
       style={{
         background:
           type == "services"
@@ -14,12 +44,21 @@ export const Parallax = ({ type }) => {
             : "linear-gradient(180deg, #111132, #505064 )",
       }}
     >
-      <h1 className="">
+      <motion.h1 style={{ y: text }}>
         {type == "services" ? "what we do?" : "what we did?"}
-      </h1>
-      <div className="mountains"></div>
-      <div className="planets"></div>
-      <div className="stars"></div>
+      </motion.h1>
+      <motion.div className="mountains"></motion.div>
+      <motion.div
+        // variants={plan}
+        // initial="initial"
+        // animate="animate"
+        style={{
+          y: planets,
+          backgroundImage: `url(${type == "services" ? "/public/planets.png" : "/public/sun.png"})`,
+        }}
+        className="planets"
+      ></motion.div>
+      <motion.div style={{ y: stars }} className="stars"></motion.div>
     </div>
   );
 };
